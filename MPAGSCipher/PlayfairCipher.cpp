029b4aee11c693cbfb29a4bdefa5d32eb70cf3e5
Thomas Latham <T.Latham@warwick.ac.uk>
2019-11-25 17:15:54 +0000

    TL review of Day 4 homework

--- MPAGSCipher/PlayfairCipher.cpp
+++ MPAGSCipher/PlayfairCipher.cpp
@@ -13,7 +13,7 @@
 PlayfairCipher::PlayfairCipher( const std::string& key )
 : key_{""}
 {
-	PlayfairCipher::setKey( key );
+	this->setKey( key );
 }
 
 std::string PlayfairCipher::applyCipher ( const std::string& inputText, const CipherMode cipherMode ) const
@@ -34,49 +34,45 @@ std::string PlayfairCipher::applyCipher ( const std::string& inputText, const Ci
 	  }
       }
 
-    std::string charPair = "AA"; //digraph
     for (size_t i = 0; i<outputText.size(); i+=2) //i+=2 to loop over 2 letters 
       {
-        std::copy(outputText.begin()+i, outputText.begin()+i+2, charPair.begin());
-
-        Coordinate coordinateOne = (*letterCoordinateMap_.find(charPair[0])).second;
-        Coordinate coordinateTwo = (*letterCoordinateMap_.find(charPair[1])).second;
+        Coordinate coordinateOne { (*letterCoordinateMap_.find(outputText[i])).second };
+        Coordinate coordinateTwo { (*letterCoordinateMap_.find(outputText[i+1])).second };
 
         // Get new coordinates - map our way to the coordinates corresponding to the new letters we should have.
-        if (coordinateOne.y == coordinateTwo.y)
+        if (coordinateOne.row == coordinateTwo.row)
 	  {		//if we are encrypting map our way to coords. Else - go the opposite way along the map.
             if (cipherMode == CipherMode::Encrypt) {
-                coordinateOne.x = (coordinateOne.x + 1) % 5;
-                coordinateTwo.x = (coordinateTwo.x + 1) % 5;
+                coordinateOne.column = (coordinateOne.column + 1) % 5;
+                coordinateTwo.column = (coordinateTwo.column + 1) % 5;
             }
             else {
-                coordinateOne.x = (coordinateOne.x - 1 + 5) % 5; 
-                coordinateTwo.x = (coordinateTwo.x - 1 + 5) % 5;
+                coordinateOne.column = (coordinateOne.column - 1 + 5) % 5; 
+                coordinateTwo.column = (coordinateTwo.column - 1 + 5) % 5;
             }
         }
-        else if (coordinateOne.x == coordinateTwo.x) {
+        else if (coordinateOne.column == coordinateTwo.column) {
             if (cipherMode == CipherMode::Encrypt) 
 	      {
-                coordinateOne.y = (coordinateOne.y + 1) % 5;
-                coordinateTwo.y = (coordinateTwo.y + 1) % 5;
+                coordinateOne.row = (coordinateOne.row + 1) % 5;
+                coordinateTwo.row = (coordinateTwo.row + 1) % 5;
 	      }
             else 
 	      {
-                coordinateOne.y = (coordinateOne.y - 1 + 5) % 5;
-                coordinateTwo.y = (coordinateTwo.y - 1 + 5) % 5;
+                coordinateOne.row = (coordinateOne.row - 1 + 5) % 5;
+                coordinateTwo.row = (coordinateTwo.row - 1 + 5) % 5;
 	      }
         }
         else 
 	  {
             //Coordinates make opposite corners of rectangle in the playfair grid.
            
-            std::swap(coordinateOne.x, coordinateTwo.x);
+            std::swap(coordinateOne.column, coordinateTwo.column);
 	  }
 
         //Find the letter associated with the new coordinates on the square
-        charPair[0] = (*coordinateLetterMap_.find(coordinateOne)).second;
-        charPair[1] = (*coordinateLetterMap_.find(coordinateTwo)).second;
-        outputText.replace(outputText.begin()+i, outputText.begin()+i+2, charPair);
+        outputText[i]   = (*coordinateLetterMap_.find(coordinateOne)).second;
+        outputText[i+1] = (*coordinateLetterMap_.find(coordinateTwo)).second;
     }
     // Return the text
     return outputText;
@@ -85,8 +81,6 @@ std::string PlayfairCipher::applyCipher ( const std::string& inputText, const Ci
 
 void PlayfairCipher::setKey( const std::string& key )
 {
-	
-	
 	const std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	key_ = key;
 	key_+=alphabet;
@@ -126,8 +120,6 @@ void PlayfairCipher::setKey( const std::string& key )
 	key_.erase(std::remove_if(key_.begin(), key_.end(),
 	[&foundChars] (unsigned char c) 
 	{
-		
-		
 		if(foundChars.find(c) == std::string::npos)
 		{
 			foundChars += c;
@@ -135,52 +127,30 @@ void PlayfairCipher::setKey( const std::string& key )
 			
 		}
 		return true;
-		
-		
-		
 	})
 	,key_.end());
 	
-	
-	
-  //Store the coordinates of each letter into a map structure
-  for (size_t i = 0; i<key_.size(); i++)
-    {
-      Coordinate coordinate = {i%5, i/5};
-      letterCoordinateMap_.insert(std::pair<char, Coordinate> {key_[i],coordinate});
-      coordinateLetterMap_[coordinate] = key_[i];
-
-    }
-	
-	
-	std::cout<< key_ << std::endl;
+	//Store the coordinates of each letter into a map structure
+	for (size_t i = 0; i<key_.size(); i++)
+	{
+		Coordinate coordinate {i%5, i/5};
+		letterCoordinateMap_[ key_[i] ] = coordinate;
+		coordinateLetterMap_[ coordinate ] = key_[i];
+	}
 }
-
-
-
 
 
 //Set up the input text for use in the encryption.
 std::string PlayfairCipher::setText(const std::string &inputText) const
 {
-  
-
-
-
-  std::string outputText = inputText;
+  std::string outputText { inputText };
   
   std::transform(outputText.begin(), outputText.end(), outputText.begin(), 
 		 [] (char i)
 		 {
-		   
-		   
-		   return (i == 'J') ? 'I' : i;
-
+		 return (i == 'J') ? 'I' : i;
 		 }
 		 );
-
-
-
 
   //if repeated characters occur add an X (Q if XX)
   for (size_t i{0}; i<outputText.size(); i+=2)
@@ -196,18 +166,12 @@ std::string PlayfairCipher::setText(const std::string &inputText) const
                 outputText.insert(i+1, "X");
 	      }
         }
-
-
     }
 
     //If the number of characters in the input string is an odd number, add a Z to the end.
     if (outputText.size() % 2 != 0){
-        outputText += "Z";
+	outputText += (outputText[outputText.size()-1] == 'Z') ? 'X' : 'Z';
     }
 
     return outputText;
-    
-
-
-
 }
--- MPAGSCipher/PlayfairCipher.hpp
+++ MPAGSCipher/PlayfairCipher.hpp
@@ -9,79 +9,75 @@
 #include "CipherMode.hpp"
 
 /**
-* \file PLayfairCipher.hpp
-*  \brief Contains the declaration of the PLayfairCipher class
-*/
-
-
-
-struct Coordinate
-{
-
-  unsigned long x,y;
-  Coordinate(unsigned long a, unsigned long b){this->x=a; this->y=b;}
-
-
-  bool operator< (const Coordinate& c) const {
-    return std::make_pair(x,y) < std::make_pair(c.x, c.y);
-  }
-
-};
-
-
-
-
-
-
+ * \file PlayfairCipher.hpp
+ *  \brief Contains the declaration of the PlayfairCipher class
+ */
 
 /**
-* \class PlayfairCipher
-* \brief Encrypt or decrypt text using the playfair cipher with the given key
-*/
+ * \class PlayfairCipher
+ * \brief Encrypt or decrypt text using the playfair cipher with the given key
+ */
 class PlayfairCipher {
 	public:
-	/**
-	 * Create a new PlayfairCipher with the given key
-	 *
-	 * \param key the key to use in the cipher
-	 */
-	explicit PlayfairCipher( const std::string& key);
-	
-	
-      /**
-     * Apply the cipher to the provided text
-     *
-     * \param inputText the text to encrypt or decrypt
-     * \param cipherMode whether to encrypt or decrypt the input text
-     * \return the result of applying the cipher to the input text
-     */
-	
-  std::string applyCipher( const std::string& inputText, const CipherMode cipherMode ) const;
-  	
-	
-	
-	
-        private:
-                /// The cipher key.
+		/**
+		 * Create a new PlayfairCipher with the given key
+		 *
+		 * \param key the key to use in the cipher
+		 */
+		explicit PlayfairCipher( const std::string& key);
+
+
+		/**
+		 * Apply the cipher to the provided text
+		 *
+		 * \param inputText the text to encrypt or decrypt
+		 * \param cipherMode whether to encrypt or decrypt the input text
+		 * \return the result of applying the cipher to the input text
+		 */
+		std::string applyCipher( const std::string& inputText, const CipherMode cipherMode ) const;
+
+	private:
+		/// The cipher key.
 		std::string key_;
-                
-                /// Key_map, char: (column, row)
-                std::map<char, Coordinate> letterCoordinateMap_;
 
-               /// Keymap., (column, row): char
-               std::map<Coordinate, char> coordinateLetterMap_;
+		/**
+		 * \struct Coordinate
+		 * \brief Holds the coordinates in the 5x5 Playfair grid
+		 */
+		struct Coordinate
+		{
+			/// The column number
+			unsigned long column;
+			/// The row number
+			unsigned long row;
 
-              /**
-              * \brief Set the key for the PlayfairCipher
-              * \param key PlayfairCipher key
-              */
-              void setKey(const std::string &key);
+			/**
+			 * \brief Need to define an ordering so they can be stored in the maps
+			 * \param c the RHS coordinate
+			 * \return true if we are less than the RHS (delegating to std::pair)
+			 */
+			bool operator< (const Coordinate& c) const {
+				return std::make_pair(column,row) < std::make_pair(c.column, c.row);
+			}
+		};
 
-             /**
-              * \brief Set up the input text for encryption
-              * \param inputText the text to encrypt
-              * \return string formatted for input to the PlayFair cipher.
-              */
-              std::string setText(const std::string  &inputText) const;
+		/// Key_map, char: (column, row)
+		std::map<char, Coordinate> letterCoordinateMap_;
+
+		/// Key_map, (column, row): char
+		std::map<Coordinate, char> coordinateLetterMap_;
+
+		/**
+		 * \brief Set the key for the PlayfairCipher
+		 * \param key PlayfairCipher key
+		 */
+		void setKey(const std::string &key);
+
+		/**
+		 * \brief Set up the input text for encryption
+		 * \param inputText the text to encrypt
+		 * \return string formatted for input to the PlayFair cipher.
+		 */
+		std::string setText(const std::string  &inputText) const;
 };
 #endif
